6d801cd43ae4c18754bc89b5a50251f578dfe543
kubersharma001 <kubersharma001@gmail.com>
2020-03-02 19:26:57 +0530

    sanders: Build missing Media|OMX packages
    Change-Id: Ieb543411e92dd00af33c5815c072a899329163eb

--- device.mk
+++ device.mk
@@ -159,8 +159,6 @@ PRODUCT_PACKAGES += \
     vendor.display.color@1.0-service \
     vendor.display.color@1.0-impl
 
-PRODUCT_PACKAGES += android.hardware.media.omx
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
@@ -251,9 +249,12 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service.sanders
 
-# Media
+# Media (OMX)
 PRODUCT_PACKAGES += \
-    libc2dcolorconvert
+    libc2dcolorconvert \
+    android.hardware.media.omx \
+    libextmedia_jni \
+    libmm-omxcore
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -299,6 +300,7 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxCore \
     libOmxEvrcEnc \
+    libOmxG711Enc \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
