486ce78f31cf875378fa76385ae2e6467dec6c58
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-04-10 01:32:30 +0200

    sanders: Update coral FP to April Security Patch
    Signed-off-by: Jorg3Lucas <jorgelucas.sd@gmail.com>

--- potato_sanders.mk
+++ potato_sanders.mk
@@ -41,10 +41,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto G (5S) Plus" \
     PRIVATE_BUILD_DESC="sanders-user 8.1.0 OPS28.65-36-14 63857 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint="google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys"
+    ro.build.fingerprint="google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
 
 # for specific
 $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
