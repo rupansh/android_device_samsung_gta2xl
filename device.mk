bb35d0287bf42c288a2a562e361e54b9fe66e8d1
kubersharma001 <kubersharma001@gmail.com>
2020-04-10 12:42:19 +0200

    sanders: Add RRO flag to device makefile and enforce all targets
    Change-Id: Ib2a49ec081bb05f4b94d5bf1a7f18fea99086c31

--- device.mk
+++ device.mk
@@ -464,6 +464,8 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+PRODUCT_ENFORCE_RRO_TARGETS :=*
+
 PRODUCT_BOARD_PLATFORM := msm8996
 
 PRODUCT_USES_QCOM_HARDWARE := true
